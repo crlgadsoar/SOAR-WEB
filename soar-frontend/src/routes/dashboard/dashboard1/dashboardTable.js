a2b35c24bbc21419e7580149c502621744e9f6af
MayankJatav <mayankjatav7898@gmail.com>
2025-03-25 14:33:25 +0530

    Implemented changes for displaying the Mitigated changes only in the dashboard

--- soar-frontend/src/routes/dashboard/dashboard1/dashboardTable.js
+++ soar-frontend/src/routes/dashboard/dashboard1/dashboardTable.js
@@ -9,7 +9,7 @@ const DashboardIncidentTable = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5002/incidents") // API call for incidents
+      .get("http://localhost:5002/incidents?status=Mitigated") // API call for incidents
       .then((response) => {
         console.log("API Response:", response.data); // Debugging line
 
