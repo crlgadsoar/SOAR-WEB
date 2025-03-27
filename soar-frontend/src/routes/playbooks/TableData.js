c866e9dd80421077b0f3e34c2b955a415a7cccf2
crlgadsoar <crlgadsoar@gmail.com>
2025-03-27 14:43:37 +0530

    Added playbook detail functionality

--- soar-frontend/src/routes/playbooks/TableData.js
+++ soar-frontend/src/routes/playbooks/TableData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Table } from "antd";
 
@@ -37,3 +37,4 @@ const PlaybookTable = () => {
 };
 
 export default PlaybookTable;
+*/
